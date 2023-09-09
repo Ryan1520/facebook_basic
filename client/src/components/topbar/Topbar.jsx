4401698081dca470b4775851332d3338d21371d8
Tran Nguyen <trandainguyen1520@gmail.com>
2023-09-09 15:17:55 +0700

    Fix rightbar

--- client/src/components/topbar/Topbar.jsx
+++ client/src/components/topbar/Topbar.jsx
@@ -39,10 +39,6 @@ export const Topbar = () => {
       </div>
 
       <div className="topbarRight">
-        {/* <div className='topbarLinks'>
-          <span className="topbarLink">Homepage</span>
-          <span className="topbarLink">Timeline</span>
-        </div> */}
         <div className="topbarIcons">
           <div className="topbarIconItem">
             <Person sx={{fontSize: '30px'}}/>
